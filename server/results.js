b4574abfb3a4eb7d05e212aa133f9212f81cf9d7
cyrillef <cyrillef@free.fr>
2017-02-22 11:06:41 +0100

    bugfix

--- server/results.js
+++ server/results.js
@@ -412,24 +412,32 @@ router.delete ('/results/:identifier', function (req, res) {
 			getLocalManifest (identifier)
 				.then (function (content) {
 					var ModelDerivative =new ForgeSDK.DerivativesApi () ;
-					ModelDerivative.deleteManifest (content.urn, forgeToken.RW, forgeToken.RW.getCredentials ()) ;
+					ModelDerivative.deleteManifest (content.urn, forgeToken.RW, forgeToken.RW.getCredentials ())
+						.catch (function (error) {
+							console.error ('Error:' + error) ;
+						}) ;
 					utils.unlink (utils.data (this.identifier + '.resultdb')) ;
 					utils.unlink (utils.data (this.identifier + '.job')) ;
 					return (getLocalFileDescriptor (this.identifier)) ;
 				}.bind ({ identifier: identifier }))
 				.then (function (desc) {	
 					var ObjectsApi =new ForgeSDK.ObjectsApi () ;
-					ObjectsApi.deleteObject (bucket, desc.objectKey, forgeToken.RW, forgeToken.RW.getCredentials ()) ;
+					ObjectsApi.deleteObject (bucket, desc.objectKey, forgeToken.RW, forgeToken.RW.getCredentials ())
+						.catch (function (error) {
+							console.error ('Error:' + error) ;
+						}) ;
 					utils.unlink (utils.data (this.identifier)) ;
 					console.log (this.identifier + " project deleted!") ;
+					res.end () ;
 				}.bind ({ identifier: identifier }))
 				.catch (function (err) {
-					console.log (err) ;
+					console.error ('Error:' + err) ;
+					res.status (500).end (err) ;
 				})
 			;
-			res.end () ;
 		})
 		.catch (function (error) {
+			console.error ('Error:' + error) ;
 			res.status (404).end () ;
 		})
 	;
