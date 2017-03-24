2e673682c7697d0c516fb1d4a273a1313298ea30
cyrillef <cyrillef@free.fr>
2017-03-24 08:54:11 +0100

    reverse code

--- server/results.js
+++ server/results.js
@@ -35,7 +35,7 @@ var sendMail =require ('./sendMail') ;
 var router =express.Router () ;
 router.use (bodyParser.json ()) ;
 
-reouter.get ('/test', function (req, res) {
+router.get ('/test', function (req, res) {
 	res.json (req.headers) ;
 }) ;
 
