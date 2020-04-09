656c36bed2f8222a95ad5a25039806370f5e1a3c
cyrillef <cyrillef@free.fr>
2020-04-09 19:25:32 +0200

    retiring website

--- server/server.js
+++ server/server.js
@@ -20,28 +20,12 @@
 var express =require ('express') ;
 var bodyParser =require ('body-parser') ;
 var favicon =require ('serve-favicon') ;
-var ejs =require ('./ejs') ;
-var forgeToken =require ('./forge-token') ; // Force loading
-var fileUpload =require ('./upload-flow') ;
-var projects =require ('./projects') ;
-var results =require ('./results') ;
-var zipsDownloads =require ('./zips-downloads') ;
-//var resultsExternals =require ('./externals') ;
-var forgeProxy =require ('./forge-proxy') ;
 
 var app =express () ;
 //app.use (bodyParser.urlencoded ({ extended: true })) ; // Support encoded bodies
 app.use (bodyParser.json ()) ;
 app.use (express.static (__dirname + '/../www')) ;
 app.use (favicon (__dirname + '/../www/favicon.ico')) ;
-app.set ('view engine', 'ejs') ;
-app.use ('/explore', ejs) ;
-app.use ('/api', fileUpload) ;
-app.use ('/api', projects) ;
-app.use ('/api', results) ;
-app.use ('/extracted/zips', zipsDownloads) ;
-//app.use ('/api', resultsExternals) ;
-app.get ('/forge-proxy/*', forgeProxy.get) ;
 
 app.set ('port', process.env.PORT || 80) ;
 
